4feb77e4251ecbf5c817ea6ca636bf3039e79e9a
Q118 <shelbyfish91@gmail.com>
2020-07-30 17:48:00 -0700

    added route to take to exercise page

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,14 +1,42 @@
 const db = require("../models");
+const app = require("express").Router();
 
 //routes
 
 module.exports = (app) => {
 
 
-    // main page
-    app.get("/api/workouts", (req, res) => {
-        res.send(index.html);
+    // main page and last workout
+    app.get("/", (req, res) => {
+        res.sendFile(_dirnam, "./index.html");
     });
   
+    // route to page for adding/updating workouts
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(__dirname, "./exercise.html"));
+      });
+
+
+    // // route to add an exercise
+    // app.put("/api/workouts/:id", (req, res) => {
+    //     db.Workout.update(  //change to exercise?
+    //       req.body,
+    //       {
+    //         where: {
+    //           id: req.body.id
+    //         }
+    //       }).then((dbWorkout) => {
+    //       res.json(dbWorkout);
+    //     });
+    //   });
+
+
+      //route to add a workout
+      app.post("/api/workouts", (req, res) => {
+        db.Workout.create(req.body).then((dbWorkout) => {
+          res.json(dbWorkout);
+          res.send(exercise.html);
+        });
+      });
   
   };
--- server.js
+++ server.js
@@ -16,7 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExertion", {
+
+// connecting to database in mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
