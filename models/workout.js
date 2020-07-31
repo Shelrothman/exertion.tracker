eac4067ed8d5e7ec5e9256ec9f8e432520073eb0
Q118 <shelbyfish91@gmail.com>
2020-07-30 18:36:25 -0700

    fixed date to default to todays date

--- models/workout.js
+++ models/workout.js
@@ -10,6 +10,7 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
 	day: {
 		type: Date,
+		default: Date.now
 	},
 	exercises: {
 		type: []
@@ -20,30 +21,3 @@ const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
 
-// Resistance WOrkout Example
-// {
-//     day: new Date(new Date().setDate(new Date().getDate() - 10)),
-//     exercises: [
-//       {
-//         type: "resistance",
-//         name: "Bicep Curl",
-//         duration: 20,
-//         weight: 100,
-//         reps: 10,
-//         sets: 4
-//       }
-//     ]
-//   }
-
-// cardio workout example
-// {
-//     day: new Date(new Date().setDate(new Date().getDate() - 7)),
-//     exercises: [
-//       {
-//         type: "cardio",
-//         name: "Running",
-//         duration: 25,
-//         distance: 4
-//       }
-//     ]
-//   }
