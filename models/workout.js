dba09a6cf44e38c867ae5f40019c5557247875b9
Q118 <shelbyfish91@gmail.com>
2020-07-30 18:40:08 -0700

    used virtual to include total duration of workouts

--- models/workout.js
+++ models/workout.js
@@ -17,6 +17,15 @@ const WorkoutSchema = new Schema({
 	},
 });
 
+
+//use virtual to set the duration to total them
+WorkoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
