db662dfa8691235f2b847b3062af6e9b5e88aeee
Q118 <shelbyfish91@gmail.com>
2020-07-30 18:43:41 -0700

    fixed duration by always using JSON on virtuals

--- models/workout.js
+++ models/workout.js
@@ -4,29 +4,32 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-
 // specifying an empty array is equivolent to 'mixed' or '[]'
-	// could choose to define the schema of each object in the exercisees array 
-const WorkoutSchema = new Schema({
-	day: {
-		type: Date,
-		default: Date.now
+// could choose to define the schema of each object in the exercisees array
+const WorkoutSchema = new Schema(
+	{
+		day: {
+			type: Date,
+			default: Date.now,
+		},
+		exercises: {
+			type: [],
+		},
 	},
-	exercises: {
-		type: []
-	},
-});
-
+	{
+		toJSON: {
+			virtuals: true,
+		},
+	}
+);
 
 //use virtual to set the duration to total them
 WorkoutSchema.virtual("totalDuration").get(function () {
-    return this.exercises.reduce((total, exercise) => {
-        return total + exercise.duration;
-    }, 0);
+	return this.exercises.reduce((total, exercise) => {
+		return total + exercise.duration;
+	}, 0);
 });
-
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
-
